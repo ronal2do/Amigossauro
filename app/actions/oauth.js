3048d324c2d669e3bdfa12f51acd07b785fd3628
Your NameRonaldo <ronal2do@gmail.com>
2016-08-15 13:24:22 -0300

    oauth

--- app/actions/oauth.js
+++ app/actions/oauth.js
@@ -7,9 +7,9 @@ import { browserHistory } from 'react-router';
 // Sign in with Facebook
 export function facebookLogin() {
   const facebook = {
-    url: 'http://159.203.179.97/auth/facebook',
+    url: 'http://198.199.87.246/auth/facebook',
     clientId: '1080139255398077',
-    redirectUri: 'http://159.203.179.97/auth/facebook/callback',
+    redirectUri: 'http://198.199.87.246/auth/facebook/callback',
     authorizationUrl: 'https://www.facebook.com/v2.5/dialog/oauth',
     scope: 'email,user_location',
     width: 580,
